68c69ff6569c9990c0b952191fc1fba66cced85d
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-05-22 15:53:15 -0400

    Add Scroll Down effect for all viewports and also replace every lorem ipsum

--- javascript/index.js
+++ javascript/index.js
@@ -1,72 +1,36 @@
-let lastScroll = 0;
-let index = 0;
-const MyDocument = window;
-// I only have article, sections and footer
-const documentChilds = Array.from(document.querySelectorAll("article, footer, section"));
-// console.log(documentChilds);
-// Hide all elements in order to display the first one
-
-hideAllElements();
-
-refactorFirstElement(documentChilds[0]);
-// console.log(MyDocument.innerHeight);
-MyDocument.addEventListener("scroll", function(){
-  let st = this.pageYOffset || document.documentElement.scrollTop;
-  if(st > lastScroll)
-  {
-    // Scroll down code
-    console.log("You Scrolled Down");
-    index++;
-    onScrollDownRefactorElements(documentChilds[index]);
-    // Now you need to get the component that is infront of the user screen
+var animateHTML = function() {
+  const elements = document.querySelectorAll('.hide');
+  const windowHeight = window.innerHeight;
+  function init() {
+    addEventHandlers();
+    checkPosition();
   }
-  else if(st == 0)
-  {
-    // documentChilds[0].classList.remove("hide");
-    // console.log(documentChilds);
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
   }
-  else {
-    // Scroll up code
-    console.log("Op, you went back up");
-    index--;
-    // onScrollUpRefactorElements(documentChilds[index]);
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+      // console.log(elements[i], positionFromTop, windowHeight);
+      // console.log(positionFromTop - windowHeight <= 10);
+      if (positionFromTop - windowHeight <= -350) {
+        elements[i].className = elements[i].className.replace(
+          'hide',
+          'fade-in-element'
+        );
+        // console.log(elements[i].className)
+      }
+    }
   }
+  return {
+    init: init
+  };
+};
 
-  lastScroll = st;
-  // console.log(document.documentElement.scrollTop);
-});
-
-function refactorFirstElement(firstElement){
-  // Display first element and get the height of everything to 100%;
-  firstElement.classList.remove("hide");
-  firstElement.style.height = `${MyDocument.innerHeight - 50}px`; 
-  firstElement.querySelector("img").style.height = `${MyDocument.innerHeight - 50}px`;
+const refactorHeight = function(){
+  elements = document.querySelectorAll('.hide');
+  Array.from(elements).map(element => element.style.height = `${window.innerHeight - 100}px`);
 }
-
-function hideAllElements()
-{
-  documentChilds.map(child => {
-    child.classList.add("hide")
-    child.style.height = `0px`;
-  });
-}
-
-function onScrollDownRefactorElements(element){
-  // debugger;
-  if(index < documentChilds.length)
-  {
-    hideAllElements();
-    element.classList.remove("hide");
-    element.style.height = `${MyDocument.innerHeight - 30}px`;
-  }
-}
-
-function onScrollUpRefactorElements(element){
-  // debugger;
-  if(index < documentChilds.length)
-  {
-    hideAllElements();
-    element.classList.remove("hide");
-    element.style.height = `${MyDocument.innerHeight - 40}px`;
-  }
-}
+refactorHeight();
+animateHTML().init();
