b2b7d1a43fc81c7e5a29eb7562985b2745181ff5
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-05-22 11:43:33 -0400

    Add scroll down effect, scroll up is not working

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,23 @@
 let lastScroll = 0;
-
+let index = 0;
 const MyDocument = window;
 // I only have article, sections and footer
 const documentChilds = Array.from(document.querySelectorAll("article, footer, section"));
 // console.log(documentChilds);
 // Hide all elements in order to display the first one
-documentChilds.map(child => child.classList.add("hide"));
+
+hideAllElements();
 
 refactorFirstElement(documentChilds[0]);
-console.log(MyDocument.innerHeight);
+// console.log(MyDocument.innerHeight);
 MyDocument.addEventListener("scroll", function(){
   let st = this.pageYOffset || document.documentElement.scrollTop;
   if(st > lastScroll)
   {
     // Scroll down code
     console.log("You Scrolled Down");
+    index++;
+    onScrollDownRefactorElements(documentChilds[index]);
     // Now you need to get the component that is infront of the user screen
   }
   else if(st == 0)
@@ -25,6 +28,8 @@ MyDocument.addEventListener("scroll", function(){
   else {
     // Scroll up code
     console.log("Op, you went back up");
+    index--;
+    // onScrollUpRefactorElements(documentChilds[index]);
   }
 
   lastScroll = st;
@@ -36,4 +41,32 @@ function refactorFirstElement(firstElement){
   firstElement.classList.remove("hide");
   firstElement.style.height = `${MyDocument.innerHeight - 50}px`; 
   firstElement.querySelector("img").style.height = `${MyDocument.innerHeight - 50}px`;
+}
+
+function hideAllElements()
+{
+  documentChilds.map(child => {
+    child.classList.add("hide")
+    child.style.height = `0px`;
+  });
+}
+
+function onScrollDownRefactorElements(element){
+  // debugger;
+  if(index < documentChilds.length)
+  {
+    hideAllElements();
+    element.classList.remove("hide");
+    element.style.height = `${MyDocument.innerHeight - 30}px`;
+  }
+}
+
+function onScrollUpRefactorElements(element){
+  // debugger;
+  if(index < documentChilds.length)
+  {
+    hideAllElements();
+    element.classList.remove("hide");
+    element.style.height = `${MyDocument.innerHeight - 40}px`;
+  }
 }
